f8c4c474d856e00c9ca625101fecef02a44a736f
Karan Pathak <karan8886@gmail.com>
2023-07-01 17:00:12 -0700

    Documented the code and added scroll lock

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,12 @@ import { useInView } from 'react-intersection-observer';
 
 function App()
 {
-      //const feeder = [{title:"onewa", creator : "twowa", pubdate : "threewa"},{title:"onema", creator : "twoma", pubdate : "threema"},{title:"oneka", creator : "twoka", pubdate : "threeka"}];
-      //this.state = {feedor: [{title:"onewa", creator : "twowa", pubdate : "threewa"},{title:"onema", creator : "twoma", pubdate : "threema"},{title:"oneka", creator : "twoka", pubdate : "threeka"}]};
-      //let [feed, setFeed] = useState([]);
       let feed = useRef([]);
-      //let [refetch, setFetch] = useState(true);
       const refetch = useRef(1);
-      let clkcall = useRef(1);
       const page = useRef(1);
-      //let [page, setPage] = useState(1);
       let [feedres, setFeedres] = useState([]);
-      //let feedres = useRef([]);
-
-      const { ref, inView, entry } = useInView({
-        /* Optional options */
+      
+      const { ref, inView } = useInView({
         delay:0,
         threshold: 0,
       });
@@ -26,19 +18,13 @@ function App()
       async function makeCall() {
         refetch.current = 0;
         const res = await fetch('http://10.0.0.190:8080');
-        console.log("Making call");
         const data = await res.json(); 
-        //data = await res.json();
-        //setFeed(data);
-        //fetcher();
         return data;
-    } ;
-    
+      } ;
+    // Fetcher decides whether to make the API call OR use the pre-fetched response to push elements for display. Fetched articles are displayed in viewport as user scrolls
     async function fetcher() {
       let element = [];
       let response = [];
-      console.log(refetch.current);
-      console.log("Entering to call");
       if (refetch.current === 1) {
         response = await makeCall();
         refetch.current = 0;
@@ -46,54 +32,31 @@ function App()
       }
       else {
         response = feed.current;
-        console.log(feed[0]);
-        console.log("Else triggered");
       }
-      //console.log("just b4 for");
-      console.log(page);
-      for (let index = 0; index < 4*page.current; index++) {
-        //console.log(response);
-        console.log("response");
+      // For loop decides how many articles are populated in one go
+      for (let index = 0; index < 40*page.current; index++) {
         element.push(response[index]);          
       }
-      console.log(element);
-      //console.log("page here");
+      // With every scroll, as the viewport changes more items are loaded
       page.current = page.current+1;
       setFeedres(element);
-      //feedres.current = element;
-      //setFetch(false);
-      //console.log(feedres[0]);
+      
     }
-
-    /*if (refetch.current = 0 && {inView}) {
-      setPage(page+1);
-      //page.current = page.current+1;
-      console.log({inView});
-      console.log("Inview");
-    }*/
+      // If the invisible box is visible postscroll, call more articles for display
     useEffect(() => {
-        //const response = async () => {await makeCall(); setFeed(response);}
-        //makeCall();
-        //if (inView) {page.current=page.current+1;}  
-        console.log("In use efect"); 
         if (inView) {fetcher();}
-        console.log(inView);      
     }, [inView]);  
     
     function onclickhandler(checker) {
-      //const feedmod = feed.current.filter(fee => fee._id !== checker); 
-      //setFeed(feedmod);
       feed.current = feed.current.filter(fee => fee._id !== checker);
-      clkcall.current = clkcall.current+1;
+      //Load n-1 items because otherwise with every click the loaded items will expand by 4x
       page.current=page.current-1;
+      //Keep the window from scrolling to top
+      const scrollPosition = window.scrollY;
       fetcher();
-      //setFetch(prevCheck => !prevCheck);
-      //console.log(feedmod);
+      window.scrollTo(0, scrollPosition);
     }
     
-    //console.log({inView}); 
-
-    //let idg = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
     return (
       <div>
       {feedres.map(fee => <Card title={fee.title} pub={fee.pub} link = {fee._id} creator = {fee.creator} desc = {fee.desc} image = {fee.image} key={Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36)} onclick={onclickhandler} />)}
@@ -105,30 +68,3 @@ function App()
   }
 
 export default App;
-
-      /*{feed.map(fee => <Card title={fee[4]} link = {fee[0]} creator = {fee[6]} pubdate={fee[5]} desc = {fee[7]} id={fee[0]} key={fee[1]} onclick={onclickhandler} />)}*/
-
-/*<h2>{`Header inside viewport ${inView}.`}</h2>*/
-//const baseURL = "/api";
-//{feed.map(fee => <Card title={fee.title} creator = {fee.creator} pubdate={fee.pubdate} onclick={onclickhandler} />)}
-
-
-//  const [post, setPost] = React.useState(null);
-
-//  React.useEffect(() => {
-//    axios.get(baseURL).then((response) => {
-//      setPost(response);
-//    });
-//  }, []);
-
-//  if (!post) return null;
-
-// <Card title="La la" creator="Ba ba" pubdate="Ja ja" />
-
-// <p className="card">"walahala"</p>
-
-    //fetch('http://localhost:8080').then((response) => { return response.json()}).then((data) => setFeed(data));
-        //console.log("Ran once");
-        //console.log(Object.keys(feed[1]));  
-      //}, []);  
-    
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,9 @@
 import React, {useState} from "react";
 import  "./index.css";
-//const cors = require('cors');
 
 function Card(props) {
-  //console.log(props.link);
-  //console.log(props.title);
-  //const [show, setShow] = useState(props);
   function likeHandler() { props.onclick(props.link);msg('l');}
   function keepHandler() { props.onclick(props.link);msg('k');}
-  //function authHandler() { props.onclick(props.link);msg('na');}
-  //function topicHandler() { props.onclick(props.link);msg('nt');}
   function itemHandler() { props.onclick(props.link);msg('ni');}
   
   function msg(pass) {
@@ -19,21 +13,18 @@ function Card(props) {
       mode: 'cors',
       body: JSON.stringify({ id: props.link, msg: pass })
     });
-    //console.log("outside fetch");
   };
-  function handleScroll() {
-    console.log('scrollTop: ');
-    }
+  //function handleScroll() {
+  //  console.log('scrollTop: ');
+  //  } onScroll={handleScroll}
   return (
-    <div onScroll={handleScroll} className="card">
+    <div  className="card">
          <img className="image" src={props.image} alt="" />
          <a href={props.link} className="name">{props.title}</a>
         <p className="info"><em>{props.pub}</em><em> - </em><em>{props.creator}</em></p>
         <p className="info">{props.desc}</p>
         <button onClick={likeHandler} className="btn" >Like</button>
         <button onClick={keepHandler} className="btn" >Keep</button>
-        {/*<button onClick={authHandler} className="btn" >~Author</button>
-        <button onClick={topicHandler} className="btn" >~Topic</button>*/}
         <button onClick={itemHandler} className="btn" >~Item</button>
 
     </div>
